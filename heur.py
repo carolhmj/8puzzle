116408133b371e0c42759b9719dfe1846a0c32a8
MarianaFL <mflgeb@gmail.com>
2015-10-26 10:41:49 -0300

    Heurística de peças no lugar errado e heurística que retorna zero adicionadas

--- heur.py
+++ heur.py
@@ -17,11 +17,24 @@ def heur_man (pz1, pz2):
 				sum = sum + (abs(i-a) + abs(j-b))
 	return sum
 
-def main():
-	i = [[0, 6, 8], [2, 7, 1], [4, 5, 3]]
-	j = [[3, 8, 6], [2, 7, 1], [4, 5, 0]]
-	print heur_man (i,j)
+def heur_min (pz1, pz2):
+	sum = 0
+	for i,pi in enumerate (pz1):
+		for j,pj in enumerate (pi):
+			if pj != 0:
+				(a,b) = find_piece(pj, pz2)
+				if (i!=a or j!=b):
+					sum = sum+1
+	return sum
+
+def heur_sane (pz1,pz2):
+	return 0
+
+# def main():
+# 	i = [[0, 6, 8], [1, 7, 2], [4, 5, 3]]
+# 	j = [[0, 6, 8], [1, 7, 2], [4, 5, 3]]
+# 	print heur_min (i,j)
 
 
-if __name__ == "__main__":
-    main()
+# if __name__ == "__main__":
+#     main()
